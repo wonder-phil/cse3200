1a51176b3c399057804bd7b76d93d1ffab7efd6f
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-04-10 11:39:12 -0400

    looper and handler update

---
+++ k2025_04_10a_looperHandler_composable/app/src/main/java/com/example/k2025_04_10a_looperhandler_composable/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.k2025_04_10a_looperhandler_composable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.k2025_04_10a_looperhandler_composable.ui.theme.K2025_04_10a_looperHandler_composableTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            K2025_04_10a_looperHandler_composableTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    K2025_04_10a_looperHandler_composableTheme {
+        Greeting("Android")
+    }
+}
