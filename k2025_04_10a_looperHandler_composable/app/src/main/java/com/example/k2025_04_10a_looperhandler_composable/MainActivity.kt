e12f8d5741b95da284b9679c2248716b17af7fbe
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-04-10 11:54:22 -0400

    looper and handler update

--- k2025_04_10a_looperHandler_composable/app/src/main/java/com/example/k2025_04_10a_looperhandler_composable/MainActivity.kt
+++ k2025_04_10a_looperHandler_composable/app/src/main/java/com/example/k2025_04_10a_looperhandler_composable/MainActivity.kt
@@ -1,47 +1,79 @@
 package com.example.k2025_04_10a_looperhandler_composable
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.k2025_04_10a_looperhandler_composable.ui.theme.K2025_04_10a_looperHandler_composableTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var simpleLooperHandler: SimpleLooperHandler
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        simpleLooperHandler = SimpleLooperHandler()
+        simpleLooperHandler.start()
+
+        var message: Message = Message()
+        message.arg1 = 9
+        message.arg2 = -55
+        //message.obj =
+
         setContent {
-            K2025_04_10a_looperHandler_composableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            UpdateText("Hello!")
             }
         }
     }
-}
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun UpdateText(textToShow: String, modifier: Modifier = Modifier) {
+    val myText = remember { mutableStateOf(textToShow) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    K2025_04_10a_looperHandler_composableTheme {
-        Greeting("Android")
+    var simpleLooperHandler = SimpleLooperHandler()
+
+    simpleLooperHandler = SimpleLooperHandler()
+    simpleLooperHandler.start()
+
+    var message: Message = Message()
+    message.arg1 = 9
+    message.arg2 = -55
+    message.obj = myText
+
+    Column(verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(0.9f)
+    ) {
+
+        Button(onClick = {
+            simpleLooperHandler.mHandler.sendMessage(message)
+            message = Message()
+            message.arg1 = 999
+            message.arg2 = -5566
+
+        }) {
+            Text(textToShow, fontSize = 32.sp)
+        }
+        Text(myText.value, fontSize = 32.sp)
     }
 }
